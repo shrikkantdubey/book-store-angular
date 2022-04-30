d26467c3aaa7a4b830a9a36f27a692d39be6ddf9
Shrikant <shri.upindia@gmail.com>
2022-04-30 13:45:25 +0530

    added angular meterial

--- bookstoreapp/src/app/app.module.ts
+++ bookstoreapp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
  
 @NgModule({
@@ -21,6 +22,7 @@ import { SharedModule } from './shared/shared.module';
     // PublicModule,
     SharedModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
 
   providers: [],
